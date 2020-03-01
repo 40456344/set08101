770d0baf202af3b60f1a94cdfb01895ee5b862a9
40456344 <40456344@napier.ac.uk>
2020-03-01 12:09:09 +0000

    Made the image a hpyertext

---
+++ hello.js
@@ -0,0 +1,13 @@
+function helloFunction(){
+	alert('Hello Napier');
+}
+
+function angletest(){
+	var angle = window.prompt("Please enter an angle: ");
+	
+	if(angle < 90) {
+	alert("The Angle you entered is acute");
+} else {
+	alert("The Angle you entered is obtuse");
+}
+}
